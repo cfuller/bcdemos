cbddc7e30d9fe465a3e0914e96ab39dbdb6ef068
Chris Fuller <chris@chrisandweeze.com>
2016-09-20 17:58:27 -0400

    adding username/lock feature

--- bell/loginForm.js
+++ bell/loginForm.js
@@ -4,19 +4,69 @@
 
 videojs.plugin('loginForm', function() {
 
-  var player = this;
-  var loginFormTemplate = '<div class="auth-form"> ' +
-    '<label for="username">Username</label> ' +
-    '<input type="text" name="username" value="" /> ' +
-    '<label for="password">Password</label> ' +
-    '<input type="password" name="password" value="" /> ' +
-    '</div>';
+  var player, overlayTemplate, loginFormTemplate, lockTemplate, playerHeight;
 
-  alert("loginForm");
+  player = this;
+  player.controls(false);
+  playerHeight = $('#' + player.id_).height();
+  overlayTemplate = '<div class="lock-overlay">{{content}}</div>';
+  loginFormTemplate = '<form class="auth-form">' +
+    '<div class="form-group">' +
+    '<label for="username">Username</label> ' +
+    '<input type="text" name="username" id="username" class="form-control" value="" /> ' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="password">Password</label> ' +
+    '<input type="password" name="password" id="password" class="form-control" value="" /> ' +
+    '</div>' +
+    '<button type="submit" class="btn btn-default login-submit">Submit</button>' +
+    '</form>';
+  lockTemplate = '<img src="img/lock-512.gif" width="100" class="lock-image" />';
+
   player.on('loadedmetadata', function() {
-    var player = this;
-    alert('metadata loaded')
-    $(player).append(loginFormTemplate)
+    var content = overlayTemplate.replace('{{content}}', lockTemplate);
+    overlay(player, content);
+    $('.lock-overlay').click(function() {
+      content = overlayTemplate.replace('{{content}}', loginFormTemplate);
+      $('.lock-overlay').remove();
+      overlay(player, content);
+      $('.auth-form').submit(function()  {
+        var data = {username: $('#username').val(), password: $('#password').val()};
+        $.ajax('login.php', {
+          data: data,
+          type: 'POST',
+          success: function(data) {
+            var username = data.replace('"', '').replace('"', '');
+            playVideo(username, player);
+          }
+        });
+        return false;
+      })
+    });
   });
 
+  function overlay(player, content) {
+    $('#' + player.id_).append(content);
+  }
+
+  function playVideo(username, player) {
+    $('.lock-overlay').remove();
+    player.controls(true);
+    player.play();
+    showUsername(username);
+    var intId = setInterval(function() {
+      showUsername(username);
+    }, 5000);
+    player.on('pause', function() {
+      clearInterval(intId);
+    });
+  }
+
+  function showUsername(username) {
+    var top = Math.floor((Math.random() * 85) + 1);
+    var left = Math.floor((Math.random() * 70) + 1);
+    var content = '<div class="username-overlay" style="top:' + top + '%; left:' + left + '%;">' + username + '</div>';
+    $('.username-overlay').remove();
+    overlay(player, content);
+  }
 });
